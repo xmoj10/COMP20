3f97dec08d71ee59969d7fa2cd5295d94824188b
Xavier Mojica <xavier.mojica@tufts.edu>
2020-03-29 15:46:36 -0400

    HW8

---
+++ HW8/playlist.js
@@ -0,0 +1,89 @@
+playlist = [
+  {
+    "artist": ["Rosalía"],
+    "title": "Dolerme",
+    "album": "Dolerme - Single",
+    "year": "2020",
+    "genre": ["Pop", "Español"],
+    "artwork": "Dolerme-artwork.jpg"
+  },
+
+  {
+    "artist": ["Lunay", "Daddy Yankee", "Bad Bunny"],
+    "title": "Soltera - Remix",
+    "album": "Épico",
+    "year": "2019",
+    "genre": ["Reggaeton", "Español"],
+    "artwork": "Soltera-artwork.jpg"
+  },
+  {
+    "artist": ["Pol Granch", "Natalia Lacunza"],
+    "title": "En Llamas",
+    "album": "En Llamas - Single",
+    "year": "2020",
+    "genre": ["Latin Pop", "Español"],
+    "artwork": "EnLlamas-artwork.jpg"
+  },
+  {
+    "artist": ["Don Patricio", "Cruz Cafuné"],
+    "title": "Contando Lunares",
+    "album": "La Dura Vida del Joven Rapero",
+    "year": "2019",
+    "genre": ["Latin Pop", "Español"],
+    "artwork": "ContandoLunares-artwork.jpg"
+  },
+
+  {
+    "artist": ["DELLAFUENTE", "C. Tengana", "Alizzz"],
+    "title": "París",
+    "album": "París - Single",
+    "year": "2019",
+    "genre": ["Latin Pop", "Español", "Rap & Hip-Hop"],
+    "artwork": "París-artwork.jpg"
+  },
+
+  {
+    "artist": ["C. Tengana", "Rosalía"],
+    "title": "Antes de Morirme",
+    "album": "Antes de Morirme - Single",
+    "year": "2016",
+    "genre": ["Trap Music", "Español", "Pop"],
+    "artwork": "AntesDeMorirme-artwork.jpg"
+  },
+
+  {
+    "artist": ["Stromae"],
+    "title": "tous les mêmes",
+    "album": "Racine Carrée",
+    "year": "2013",
+    "genre": ["Dance & Electronic", "Français"],
+    "artwork": "TousLesMêmes-artwork.jpg"
+  },
+
+  {
+    "artist": ["Nao"],
+    "title": "Bad Blood",
+    "album": "For All We Know",
+    "year": "2016",
+    "genre": ["R&B"],
+    "artwork": "BadBlood-artwork.jpg"
+  },
+
+  {
+    "artist": ["Paloma Mami"],
+    "title": "Fingías",
+    "album": "Fingías - Single",
+    "year": "2019",
+    "genre": ["Latin Hip-Hop", "Español", "Pop"],
+    "artwork": "Fingías-artwork.jpg"
+  },
+
+  {
+    "artist": ["Ana Guerra"],
+    "title": "Bajito",
+    "album": "Reflexión",
+    "year": "2019",
+    "genre": ["Pop", "Español"],
+    "artwork": "Bajito-artwork.jpg"
+  }
+]
